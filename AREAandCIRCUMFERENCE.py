7b0ada384ec3d0e22815756b50fd646b9385e82a
Jeshva-Praveen <23pa1a05j9@vishnu.edu.in>
2024-09-23 16:55:25 +0530

    Create AREAandCIRCUMFERENCE.py

---
+++ AREAandCIRCUMFERENCE.py
@@ -0,0 +1,7 @@
+#python program to find area and circumference of a circle
+r=int(input("Enter radius of circle:"))
+pi=3.14
+Area=pi*r*r
+print("Area of circle is:",Area,"sqkm")
+Circumference=2*pi*r
+print("Circumference of circle is:",Circumference,"cm")
