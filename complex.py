da40347b418a5956f4114e33450bf1d4b8b19413
Jeshva-Praveen <23pa1a05j9@vishnu.edu.in>
2024-09-23 16:52:29 +0530

    Create complex.py

---
+++ complex.py
@@ -0,0 +1,8 @@
+#python program for multpily  and add complex numbers
+print("enter the complex number in a+bj format!")
+a1=complex(input('Enter first complex number:'))
+b1=complex(input('Enter second complex number:'))
+sum=a1+b1
+print("sum of two complex numbers:",sum)
+product=a1*b1
+print("product of two complex numbers:",product)
