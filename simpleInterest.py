644eb70e97df4ef204e5c1d63f8e2b65d87ebaaf
Jeshva-Praveen <23pa1a05j9@vishnu.edu.in>
2024-09-23 16:54:12 +0530

    Create simpleInterest.py

---
+++ simpleInterest.py
@@ -0,0 +1,6 @@
+#python program for finding simple interest
+p=int(input("Enter principle amount:"))
+r=float(input("Enter rate of interest:"))
+t=int(input("Enter time period:"))
+Simple_interest =(p*t*r)/100
+print("Simple Interest is :",Simple_interest)
