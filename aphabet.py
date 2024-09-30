dd5d1765a12d1fb63ceba68af575c34cbe919ec7
Jeshva Praveen <176747779+Jeshva-Praveen@users.noreply.github.com>
2024-09-30 15:25:53 +0530

    Create aphabet.py

---
+++ aphabet.py
@@ -0,0 +1,6 @@
+#python program to check alphabet is vowel or consonant
+a=input("Enter a character:")
+if a=='a' or a=='e' or a=='i' or a=='o' or a=='u'or a=='A' or a=='E' or a=='I' or a=='O' or a=='U':
+  print("Given character",a,"is a Vowel")
+else:
+  print("Given character",a,"is a Consonant")
