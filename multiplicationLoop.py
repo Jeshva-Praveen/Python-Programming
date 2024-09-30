d670dc235bb2720d141a9d79a3c0fca73d44df07
Jeshva Praveen <176747779+Jeshva-Praveen@users.noreply.github.com>
2024-09-30 15:36:43 +0530

    Update multiplicationLoop.py

--- multiplicationLoop.py
+++ multiplicationLoop.py
@@ -4,3 +4,7 @@ i=1
 while i<=10:
   print(n,'X',i,'=',n*i)
   i+=1
+#Python program to multiplication using for loop
+n=int(input('Enter a number:'))
+for i in range(1,11):
+  print(n,'X',i,'=',n*i)
